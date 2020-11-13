f158311ce1952fa8675d5ae2912fe1a5a25c7977
developinghands <nikhilmishra262@gmail.com>
2020-11-13 23:39:08 +0530

    Discover section fully functional

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "./Discover.css";
 import { db } from "../firebase";
+import Suggested from "./Suggested";
+import Post from "./Post";
 
 export default function Discover() {
   const [list, setList] = useState([]);
+  const [show, setShow] = useState(false);
+  const [categoryList, setCategoryList] = useState([]);
+  const [newPost, setNewPost] = useState("");
+  const [post, loadPost] = useState("");
   useEffect(() => {
     db.collection("discover")
       .orderBy("category")
-      .get()
-      .then(snap =>
+      .onSnapshot(snap =>
         setList(
           snap.docs.map(doc => ({
             id: doc.id,
@@ -16,12 +21,61 @@ export default function Discover() {
           }))
         )
       );
-  });
+  }, []);
+  const openList = category => {
+    setShow(true); // show category related posts
+    console.log("openList called with ", category);
+    db.collection("news")
+      .where("category", "==", `${category}`)
+      .get()
+      .then(snap =>
+        setCategoryList(
+          snap.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data()
+          }))
+        )
+      )
+      .catch(error => console.error(error));
+    console.log("Data Stored", categoryList);
+  };
+  const setPost = postId => {
+    console.log("Recieved new Post Data..."); //console
+    categoryList.map(post => {
+      if (post.id == postId) {
+        setNewPost(postId);
+        console.log("New Post State Updated...", newPost); //console
+      }
+    });
+  };
   return (
     <div className="discover">
-      {list.map(item => (
-        <p className="block" key={item.id}>{item.data.category}</p>
-      ))}
+      {!show ? (
+        list.map(item => (
+          <p
+            className="block"
+            key={item.id}
+            onClick={() => {
+              openList(item.data.category);
+            }}
+          >
+            {item.data.category}
+          </p>
+        ))
+      ) : !newPost ? (
+        categoryList.map(post => (
+          <div className="suggestedBlock">
+          <Suggested
+            key={post.id}
+            id={post.id}
+            title={post.data.title}
+            url={post.data.image}
+            setPost={setPost}
+          />
+          </div>
+        ))
+      ) : <Post key={newPost} id={newPost}/>
+      }
     </div>
   );
 }
